6be61e31b3af69daa56d3bee993fd3ce6d90c892
Camille Bachmann <squirrel.fsx@gmail.com>
2023-07-15 17:56:20 +0200

    Reading multiple frames when multiple ones are available

--- adsbframereader.cpp
+++ adsbframereader.cpp
@@ -42,95 +42,98 @@ void ADSBFrameReader::connectionEstablished()
 
 void ADSBFrameReader::onReadyRead()
 {
-    QString line = tcpSocket.readLine();
-    QStringList data = line.split(",");
-    if(data.size() == adsbFrameDescr.size)
+    while(tcpSocket.canReadLine())
     {
-        QString icao = data.at(adsbFrameDescr.icao);
-        if(icao != "" && icao != "000000")
+        QString line = tcpSocket.readLine();
+        QStringList data = line.split(",");
+        if(data.size() == adsbFrameDescr.size)
         {
-            int i=0;
-            while(i<seenAcf->size()) // remove acf not seen for a while
+            QString icao = data.at(adsbFrameDescr.icao);
+            if(icao != "" && icao != "000000")
             {
-                int awayTime = settings->getMinutesBeforeRemoved()*60;
-                if(seenAcf->at(i).getIcao() == icao)
+                int i=0;
+                while(i<seenAcf->size()) // remove acf not seen for a while
                 {
-                    (*seenAcf)[i].seen();
-                    i++;
-                }
-                else if(seenAcf->at(i).notSeenForSec() > awayTime && interestingAcf->indexOfIcao(icao) < 0)
-                {
-                    seenAcf->removeAt(i);
-                    userInterface->redrawAcfList();
-                }
-                else
-                {
-                    i++;
-                }
-            }
-
-            if(!seenAcf->contains(icao)) // new aircraft
-            {
-                AircraftADSB acf = AircraftADSB(icao);
-                if(interestingAcf->contains(icao)) // get info from database
-                {
-                    acf = interestingAcf->at(interestingAcf->indexOfIcao(icao));
+                    int awayTime = settings->getMinutesBeforeRemoved()*60;
+                    if(seenAcf->at(i).getIcao() == icao)
+                    {
+                        (*seenAcf)[i].seen();
+                        i++;
+                    }
+                    else if(seenAcf->at(i).notSeenForSec() > awayTime && interestingAcf->indexOfIcao(icao) < 0)
+                    {
+                        seenAcf->removeAt(i);
+                        userInterface->redrawAcfList();
+                    }
+                    else
+                    {
+                        i++;
+                    }
                 }
 
-                seenAcf->append(acf);
-                userInterface->updateListInfo(icao, IcaoClmn, icao);
-
-                if(interestingAcf->contains(icao)) // alarm
+                if(!seenAcf->contains(icao)) // new aircraft
                 {
-                    seenAcf->back().setAlertStatus(AlertRaised);
-                    userInterface->raiseAlert(icao);
-                }
-            }
+                    AircraftADSB acf = AircraftADSB(icao);
+                    if(interestingAcf->contains(icao)) // get info from database
+                    {
+                        acf = interestingAcf->at(interestingAcf->indexOfIcao(icao));
+                    }
 
-            AircraftADSB *currentAircraft = &(*seenAcf)[seenAcf->indexOfIcao(icao)];
-            if(data.at(adsbFrameDescr.call) != "")
-            {
-                QString callsign = data.at(adsbFrameDescr.call);
-                currentAircraft->setCallsignLive(callsign);
-                userInterface->updateListInfo(icao, CallsignClmn, callsign);
+                    seenAcf->append(acf);
+                    userInterface->updateListInfo(icao, IcaoClmn, icao);
+
+                    if(interestingAcf->contains(icao)) // alarm
+                    {
+                        seenAcf->back().setAlertStatus(AlertRaised);
+                        userInterface->raiseAlert(icao);
+                    }
+                }
+
+                AircraftADSB *currentAircraft = &(*seenAcf)[seenAcf->indexOfIcao(icao)];
+                if(data.at(adsbFrameDescr.call) != "")
+                {
+                    QString callsign = data.at(adsbFrameDescr.call);
+                    currentAircraft->setCallsignLive(callsign);
+                    userInterface->updateListInfo(icao, CallsignClmn, callsign);
+                }
+                if(data.at(adsbFrameDescr.alti) != "")
+                {
+                    int alti = data.at(adsbFrameDescr.alti).toInt();
+                    currentAircraft->setAltitude(alti);
+                    userInterface->updateListInfo(icao, AltiClmn, QString::number(alti));
+                }
+                if(data.at(adsbFrameDescr.spd) != "")
+                {
+                    int spd = data.at(adsbFrameDescr.spd).toInt();
+                    currentAircraft->setSpeed(spd);
+                }
+                if(data.at(adsbFrameDescr.hdg) != "")
+                {
+                    int hdg = data.at(adsbFrameDescr.hdg).toInt();
+                    currentAircraft->setHeading(hdg);
+                }
+                if(data.at(adsbFrameDescr.lat) != "")
+                {
+                    float lat = data.at(adsbFrameDescr.lat).toFloat();
+                    currentAircraft->setLatitude(lat);
+                }
+                if(data.at(adsbFrameDescr.lon) != "")
+                {
+                    float lon = data.at(adsbFrameDescr.lon).toFloat();
+                    currentAircraft->setLongitude(lon);
+                }
+                if(data.at(adsbFrameDescr.vspd) != "")
+                {
+                    int vSpd = data.at(adsbFrameDescr.vspd).toInt();
+                    currentAircraft->setVerticalSpeed(vSpd);
+                }
+                if(data.at(adsbFrameDescr.sqwk) != "")
+                {
+                    int sqwk = data.at(adsbFrameDescr.sqwk).toInt();
+                    currentAircraft->setSqawk(sqwk);
+                }
+                userInterface->updateLiveInfo();
             }
-            if(data.at(adsbFrameDescr.alti) != "")
-            {
-                int alti = data.at(adsbFrameDescr.alti).toInt();
-                currentAircraft->setAltitude(alti);
-                userInterface->updateListInfo(icao, AltiClmn, QString::number(alti));
-            }
-            if(data.at(adsbFrameDescr.spd) != "")
-            {
-                int spd = data.at(adsbFrameDescr.spd).toInt();
-                currentAircraft->setSpeed(spd);
-            }
-            if(data.at(adsbFrameDescr.hdg) != "")
-            {
-                int hdg = data.at(adsbFrameDescr.hdg).toInt();
-                currentAircraft->setHeading(hdg);
-            }
-            if(data.at(adsbFrameDescr.lat) != "")
-            {
-                float lat = data.at(adsbFrameDescr.lat).toFloat();
-                currentAircraft->setLatitude(lat);
-            }
-            if(data.at(adsbFrameDescr.lon) != "")
-            {
-                float lon = data.at(adsbFrameDescr.lon).toFloat();
-                currentAircraft->setLongitude(lon);
-            }
-            if(data.at(adsbFrameDescr.vspd) != "")
-            {
-                int vSpd = data.at(adsbFrameDescr.vspd).toInt();
-                currentAircraft->setVerticalSpeed(vSpd);
-            }
-            if(data.at(adsbFrameDescr.sqwk) != "")
-            {
-                int sqwk = data.at(adsbFrameDescr.sqwk).toInt();
-                currentAircraft->setSqawk(sqwk);
-            }
-            userInterface->updateLiveInfo();
         }
     }
     // connect again if no message for N min
